848a3486df84059c740e879ba965e82e24e2cfe6
Tan <bivani2805@gmail.com>
2025-02-17 02:31:20 +0200

    Progress for check

--- app/src/main/java/com/exampl/reserver/FloorPlan.java
+++ app/src/main/java/com/exampl/reserver/FloorPlan.java
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -20,6 +23,10 @@ public class FloorPlan extends AppCompatActivity {
 
     private TextView mTest;
 
+    private LinearLayout table_one;
+    private LinearLayout table_two;
+    private LinearLayout table_three;
+
     public static final String USER_INPUT = "userInput";
 
     @SuppressLint("MissingInflatedId")
@@ -28,8 +35,26 @@ public class FloorPlan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor_plan);
 
+        table_one = findViewById(R.id.table_one);
+        table_two = findViewById(R.id.table_two);
+        table_three = findViewById(R.id.table_three);
+
         Intent intent = getIntent();
         String theInput = intent.getStringExtra(USER_INPUT);
+
+        if (!theInput.equals("")) {
+            int guests = Integer.parseInt(theInput);
+            if (guests > 4) {
+                for (int i = 0; i < table_one.getChildCount(); i++) {
+                    View view = table_one.getChildAt(i);
+
+                    if (view instanceof ImageView) {
+                        ((ImageView) view).setImageResource(R.drawable.round_solid);
+                    }
+                }
+            }
+        }
+
     }
 
 }
